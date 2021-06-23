ddbc3a7cdda748320d26b86d50905cb082e89c84
mask-506 <2500130018@qq.com>
2021-06-23 08:56:03 +0800

    test mask-506

--- src/top/mask/test/TestGit.java
+++ src/top/mask/test/TestGit.java
@@ -3,5 +3,6 @@ package top.mask.test;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("test git");
+        System.out.println("test git2");
     }
 }
